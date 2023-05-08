53cfc338395e28ae1e5175b3c6caa34e54dbd9f1
Ramona Prower <garfieldfan3993@gmail.com>
2023-05-08 13:36:24 +0100

    Can't have spaces in names

--- commands/commands/vibe.js
+++ commands/commands/vibe.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-		.setName('vibe check')
+		.setName('vibecheck')
 		.setDescription('checking meow\'s vibes'),
 	async execute(interaction, guildUserCat) {
         const reply = await guildUserCat.getReaction('vibecheck');
